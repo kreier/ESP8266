50af6580916e3ab35017f5476499be486c156bb8
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2020-01-29 10:27:41 +0700

    Update display_5x7.ino

--- arduino_c/display_5x7/display_5x7.ino
+++ arduino_c/display_5x7/display_5x7.ino
@@ -3,7 +3,7 @@
 #include <U8g2lib.h>
 
 //U8G2_SH1106_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 12, /* data=*/ 14);
-U8G2_SH1106_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 12, /* data=*/ 14);
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 12, /* data=*/ 14, /* reset=*/ U8X8_PIN_NONE);
 
 void setup() {
   u8g2.begin();
@@ -13,10 +13,14 @@ void loop() {
   u8g2.firstPage();
   do {
     u8g2.setFont(u8g2_font_5x7_tf);
-    u8g2.drawStr(0,7,"Hello World!");
-    u8g2.drawStr(0,15,"abcdefghijklmnopqrstuvwxyz");
-    u8g2.drawStr(0,23,"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    u8g2.drawStr(0,31,"01234568790123465789012345");
+    u8g2.drawStr(0, 7,"Lorem ipsum dolor sit est,");
+    u8g2.drawStr(0,15,"consectetur elit, sed do");
+    u8g2.drawStr(0,23,"ut labore et dolore magna");
+    u8g2.drawStr(0,31,"aliqua. Et malesuada fames");
+    u8g2.drawStr(0,39,"ac turpis egestas integer");
+    u8g2.drawStr(0,47,"eget aliquet nibh. Ipsum");
+    u8g2.drawStr(0,55,"suspendisse ultrices gravi");
+    u8g2.drawStr(0,64,"da dictum fusce. Ac felis");
   } while ( u8g2.nextPage() );
   delay(1000);
 
